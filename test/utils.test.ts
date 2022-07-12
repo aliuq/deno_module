a016841a2b8f889780f784b8c60fb1022ac94870
liuq <lqadm@qq.com>
2022-07-12 16:52:42 +0800

    test: increase `sleep` tolerance on MacOS

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -5,5 +5,5 @@ Deno.test('Function - sleep', async () => {
   const startTime = new Date().getTime()
   await sleep(2000)
   // Allow 5ms tolerance
-  assertAlmostEquals(new Date().getTime() - startTime, 2000, 5)
+  assertAlmostEquals(new Date().getTime() - startTime, 2000, 50)
 })
